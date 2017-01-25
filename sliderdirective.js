97cd906a0f73477d9f1163727d54e3e711b8a71c
Justin Unbedacht <Justin Unbedacht>
2017-01-25 12:41:20 -0700

    testing

---
+++ sliderdirective.js
@@ -0,0 +1,12 @@
+angular.module("app").directive("sliderDirective", function() {
+    return {
+        restrict: "EA",
+        templateUrl: "./slick.html",
+        link: function(scope, element, attribute) {
+            $(document).ready(function() {
+                $('.single-item').slick();
+            });
+        }
+    };
+
+});
